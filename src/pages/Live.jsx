2709a576b3902cd0af68e44105eb3ec69bce38c6
Cyusa Munezero Keny <gasananike@gmail.com>
2021-10-31 21:34:03 +0200

    Continuation...

---
+++ src/pages/Live.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class LivePage extends React.Component {
+    render() { 
+        return <div>
+            <p>Live</p>
+        </div>;
+    }
+}
+ 
+export default LivePage;
