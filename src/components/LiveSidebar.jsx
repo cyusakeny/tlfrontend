79967ba4c652a1c68809025c8360b7a2f4a4b565
Cyusa Munezero Keny <gasananike@gmail.com>
2021-11-02 07:27:16 +0200

    development of live session

--- src/components/LiveSidebar.jsx
+++ src/components/LiveSidebar.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import LiveCompetitions from './LiveCompetions';
 const  LiveSidebar = (props)=>{
     return(
-        <div className="">
-        <p> Frenchlick</p>
+        <div className="border-2 border-black bg-gray-100 flex flex-col justify between w-72 space-y-2 overflow-hidden h-full absolute">
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
+       <LiveCompetitions/>
         </div>
     );
 }
--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import LiveSidebar from '../components/LiveSidebar';
 class LivePage extends Component {
     render() { 
-        return <div>
+        return <div className="flex flex-row bg-gray  bg-gray-300">
             <LiveSidebar/>
+            <div className="ml-80 relative">
+                
+<input type="text" name="" id="" placeholder="&#xF199;Search" className="outline-none"/>
+            </div>
+       <div className="bg-red-300 ml-80 h-96">
+           kenyy
+       </div>
         </div>;
     }
 }
