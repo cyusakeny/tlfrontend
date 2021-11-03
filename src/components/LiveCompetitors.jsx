19fbc1ff3440f7d5f7d818d5e7d1e80e36476a17
Cyusa Munezero Keny <gasananike@gmail.com>
2021-11-03 07:34:26 +0200

    live components

---
+++ src/components/LiveCompetitors.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ProfileImage from'/home/keny/tlfrontend/src/images/stephen-curry.jpg'
+const LiveCompetitors=(props)=>{
+    return(
+        <div className="flex flex-row gap-4 text-gray-700 border border-white hover:border-gray-300
+         hover:shadow-md rounded-md">
+            <p className="pt-1  ml-2 w-10 text-center">1</p>
+            <Link className="ml-2  rounded-xl pt-1 w-44 h-10   ">
+                <div className=" flex flex-row w-36 ">
+               <img src={ProfileImage} alt="Profile" className="rounded-full w-8 h-8"/>
+               <h1 className="text-black text-sm -mt-0.5 ml-1">Maestro</h1>
+               <p className="mt-4 -ml-12 text-xs text-gray-600">Pro User</p>
+                </div>
+                </Link>
+    <p className="pt-1   w-24">34 WPM</p>
+    <p className="pt-1   w-10">90%</p>
+    <p className="pt-1  ml-4 w-20">21 words</p>
+    <p className="pt-1  ml-4 w-24">Rwanda</p>
+    <p className="pt-1  ml-4 w-32">Junior Typist</p>
+    <p className="pt-1  ml-4 w-56">"Ready to Rumble "</p>
+        </div>
+    );
+}
+export default LiveCompetitors;
--- src/components/LiveSidebar.jsx
+++ src/components/LiveSidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LiveCompetitions from './LiveCompetions';
 const  LiveSidebar = (props)=>{
     return(
-        <div className="border-2 border-black bg-gray-100 flex flex-col justify between w-72 space-y-2 overflow-hidden h-full absolute">
+        <div className=" bg-gray-100 flex flex-col justify between w-64 space-y-2 overflow-hidden h-full absolute">
        <LiveCompetitions/>
        <LiveCompetitions/>
        <LiveCompetitions/>
