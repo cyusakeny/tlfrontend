74217e3ba106135060842218634cc21f02396d8d
Cyusa Munezero Keny <gasananike@gmail.com>
2021-11-03 18:10:34 +0200

    Live Sidebar continuation

--- src/components/LiveCompetions.jsx
+++ src/components/LiveCompetions.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Profile from'/home/keny/tlfrontend/src/images/stephen-curry.jpg';
 const LiveCompetitions = (props)=>{
     return(
-      <div className="flex flex-row space-x-4  ml-4 w-48 pl-0.5 text-gray-600 rounded-md py-2 hover:bg-white hover:shadow-md hover:border-1">
+      <div className="flex flex-row space-x-4  ml-8 w-48 pl-0.5 text-gray-600 rounded-md py-2 hover:bg-white hover:shadow-md hover:border-1">
           <img src={Profile} alt="ImageProfile" className="rounded-full w-8 h-7"/>
-         <p className="text-base">French lick</p> 
+         <p className="text-base">{props.name}</p> 
           </div>
     );
 }
