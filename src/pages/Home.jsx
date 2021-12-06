ba430a0b864a29d4f1d1fced1b4249a408e591e8
Cyusa Munezero Keny <gasananike@gmail.com>
2021-12-06 14:15:19 +0200

    Continuation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,26 +11,35 @@ class HomePage extends Component {
         return (  
             <div className="bg-black overflow-hidden h-full w-full absolute left-0">
             <Navbar/>
-            <div className="sm:pb-8 pb-2 sm:pl-20 pl-1 sm:mt-20 mt-1 sm:ml-96 ml-2"> 
-              <p className="sm:ml-64 ml-1  sm:text-9xl text-7xl font-sans text-white">TYPING</p>
-              <p className="sm:mt-8 sm:ml-96 ml-10 sm:text-7xl text-5xl text-yellow-color font-postcolombo">LORD</p>
-              <p className="sm:mt-16 mt-12 sm:ml-72  ml-1 text-white text-3xl">"With HardWork and Passion"</p>
+            {/* Typing lord and quote */}
+            <div className="sm:pb-8 pb-2 sm:pl-20 pl-1 sm:mt-12 mt-1 sm:ml-96 ml-2"> 
+              <p className="sm:ml-64 ml-1  sm:text-7xl text-7xl font-sans text-white">TYPING</p>
+              <p className="sm:-mt-16.3 sm:ml-98 ml-10 sm:text-7xl text-3xl text-yellow-color font-postcolombo">LORD</p>
+              <p className="sm:mt-8 mt-12 sm:ml-63 w-97 ml-1 text-white text-xl">Learn How To Type Wherever you are  However you are and Whoever
+              you are Whatever you do just do it with hardwork and passion Give your one hundred percent something will workout in the end
+              </p>
             </div>
-            <div className="sm:-mt-80 mt-2 sm:pb-8 pb-12">
-            <img src={keyimage} className="sm:ml-20 sm:w-600 sm:h-275 w-72 h-28 ml-4 "  alt="keyboardimage" />
+            {/* Image */}
+            <div className="sm:-mt-60 mt-2 sm:pb-8 pb-12">
+            <img src={keyimage} className="sm:ml-20 sm:w-600 sm:h-295 w-72 h-28 ml-4"  alt="keyboardimage" />
             </div>
-            <div className="flex flex-row  sm:ml-20 sm:mt-24 mt-4 ml-12">
-            <Link to=""> <FacebookIcon className="fill-current text-yellow-color w-12 h-12 mr-12"/></Link> 
-            <Link to=""><InstaIcon className="fill-current text-yellow-color w-12 h-12 mr-12"/></Link>
-            <Link to=""><TwitterIcon className="fill-current text-yellow-color w-12 h-12 mr-12"/></Link>
+            {/* Social medias */}
+            <div className="flex flex-row  sm:ml-20 sm:mt-16 ml-12">
+            <Link to=""> <FacebookIcon className="fill-current text-yellow-color w-6 h-6 mr-12"/></Link> 
+            <Link to=""><InstaIcon className="fill-current text-yellow-color w-6 h-6 mr-12"/></Link>
+            <Link to=""><TwitterIcon className="fill-current text-yellow-color w-6 h-6 mr-12"/></Link>
             </div>
-            <div className=" flex sm:justify-end justify-start sm:mr-96 mr-0 sm:-mt-16 mt-2 sm:pb-8 pb-16">
-            <div className=" relative pl-12 pr-4 sm:ml-0 ml-8 mt-4 bg-yellow-color rounded-sm">
-            <Link to="" className="text-3xl"><PhoneIcon className="fill-current text-black w-8 h-8 absolute right-40 insest-y-1"/>
-             Contact us</Link>
-            </div>
-            </div>
-            </div>
+            {/* Contact us */}
+             <div className="-mt-36  float-right flex justify-end mr-36 space-x-8 ">
+            <Link to="" className=" border w-56 px-1.5 border-yellow-color  pt-1 relative 
+            bg-yellow-color text-black flex flex-row hover:bg-black hover:text-white rounded">
+            <p className="font-mono text-3xl">Get started</p> </Link>
+            <Link to="" className="border w-52 border-yellow-color  pl-7 pr-2  pt-0.5 relative 
+            rounded-3xl bg-yellow-color text-black flex flex-row hover:bg-black hover:text-white">
+                <PhoneIcon className="fill-current  w-6 h-7 absolute top-1 left-2.5"/>
+            <p className="font-mono text-3xl">Contact</p> </Link>
+             </div>
+                       </div>
         );
     }
 }
